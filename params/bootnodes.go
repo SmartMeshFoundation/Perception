14f854c7e8da4f48b2a60654de7456189abef3a1
liangchuan <cc14514@icloud.com>
2018-12-20 12:45:17 +0800

    append bootnodes

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -1,5 +1,10 @@
 package params
 
 var Bootnodes = []string{
-	//TODO 添加 bootnode 之前只能通过 console 进行 connect 来测试
+	// qingdao
+	"/ip4/47.104.232.24/tcp/40001/ipfs/QmbJdL9WRFdixhFcq1eFdUo7mpYazuBa5EQVkdZ1YRNGTR",
+	// shanghai
+	"/ip4/47.100.215.147/tcp/40001/ipfs/QmPxJ2VQBDAjLhzVxctJQq4wr2YXMRKdQC6Dpo5u73NPud",
+	// hongkong
+	"/ip4/47.75.5.197/tcp/40001/ipfs/QmRCjEDzRNqYn28csc9uTLcJ1FHAuVGQsoLXtGJ4Qhmx8Q",
 }
